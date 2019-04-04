740249d201d61918086af0afbf1435ece09eb993
Samuel Ocbazghi <ocb013@yahoo.com>
2019-04-04 15:22:52 -0400

    another test

--- app/src/androidTest/java/com/test/vizzy/VizzySnapshotTest.kt
+++ app/src/androidTest/java/com/test/vizzy/VizzySnapshotTest.kt
@@ -1,4 +1,19 @@
 package com.test.vizzy
 
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class VizzySnapshotTest {
+
+    @get:Rule
+    val testRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun takePicOfHomescreen() {
+        Thread.sleep(1000)
+    }
 }
